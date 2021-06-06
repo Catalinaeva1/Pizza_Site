5c67e277733d9a46e2cde5b84801d77dad7864ff
Catalinaeva1 <gravesalicia@hotmail.com>
2021-06-05 20:21:31 -0500

    Uploading all files

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SidebarContainer, 
+    Icon, 
+    CloseIcon, 
+    SidebarMenu, 
+    SidebarLink,
+    SidebarRoute, 
+    SideBtnWrap 
+    } from './SidebarElements';
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <div>
+            <SidebarContainer isOpen={isOpen} onClick={toggle}>
+                <Icon onclick="{toggle}">
+                    <CloseIcon/>
+                </Icon>
+                <SidebarMenu>
+                    <SidebarLink to="/">Pizzas</SidebarLink>
+                    <SidebarLink to="/">Desserts</SidebarLink>
+                    <SidebarLink to="/">Full Menu</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to ="/">Order Now</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarContainer>
+        </div>
+    )
+}
+
+export default Sidebar
