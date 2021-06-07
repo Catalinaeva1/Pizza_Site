9dd796aa99ad47067f69b63426a5ecc95914a32f
Catalinaeva1 <gravesalicia@hotmail.com>
2021-06-07 12:46:50 -0500

    Updated some css

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -1,27 +1,26 @@
 import styled from 'styled-components';
 
-
 export const ProductsContainer = styled.div`
-    width: 100vw;
-    min-height: 100vh;
-    padding: 5rem calc((100vw - 1300px) /2);
-    background: #150f0f;
-    color: #fff;
+  /* width: 100vw; */
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: #150f0f;
+  color: #fff;
 `;
 
-
 export const ProductWrapper = styled.div`
-    display: flex;
-    flex-wrap;
-    justify-content: center;
-    margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
 `;
 
 export const ProductCard = styled.div`
-    margin: 0 2rem;
-    line-height: 2;
-    width: 300px;
+  margin: 0 2rem;
+  line-height: 2;
+  width: 300px;
 `;
+
 export const ProductImg = styled.img`
   height: 300px;
   min-width: 300px;
@@ -65,7 +64,7 @@ export const ProductButton = styled.button`
   background: #e31837;
   color: #fff;
   transition: 0.2 ease-out;
-  
+
   &:hover {
     background: #ffc500;
     transition: 0.2s ease-out;
